89727a88ce5a54ed5f3d60f0a57e3898c3981c0f
Udit Nayak <UDIT.NAYAK@ibm.com>
2026-06-19 17:04:34 +0530

    Some changes from loki.

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -59,7 +59,7 @@ public class HelloWorldModel {
     }
 
     /**
-     * Test changes for the jira ticket: ADCMS-3
+     * Test changes for the jira ticket: ADCMS-3 - Sling Model - @ValueMapValue annotation
      */
     public String getMessage() {
         return message;
